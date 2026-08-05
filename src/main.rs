b397fc61e8b38afa4ac04183a982c63c53d15264
Miguel Loranca <mloranca703@gmail.com>
2026-08-04 23:13:48 -0600

    finished the match, but in the table

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ enum Node {
     Quote(String),
 
     Table {
+        // TODO: should be row
         cols: Vec<Vec<String>>,
     },
 }
@@ -134,11 +135,47 @@ fn parse(input: &str) -> Ast {
 }
 
 fn render(ast: &Ast) -> String {
-    let html = String::new();
+    let mut html = String::new();
 
-    for &item in ast.document.iter() {
+    for item in ast.document.iter() {
         match item {
-            Node::Heading => html.push_str(""),
+            Node::Heading { level, text } => html.push_str(&format!("<h{level}>{text}</h<level>")),
+            Node::Paragraph(t) => html.push_str(&format!("<p>{t}</p>")),
+            Node::List { ordered: _, items } => {
+                html.push_str(&format!("<ul>"));
+
+                for s in items {
+                    html.push_str(&format!("<li>{s}</li>"));
+                }
+
+                html.push_str(&format!("</ul>"));
+            }
+            Node::Code {
+                language,
+                code_lines,
+            } => html.push_str(&format!(
+                "<pre><code = class=\"language-{language}\">{code_lines}</code></pre>"
+            )),
+            Node::Quote(t) => html.push_str(&format!("<q>{t}</q>")),
+            Node::Table { cols } => {
+                html.push_str(&format!("<div class=\"table-wrapper\"><table><thead><tr>"));
+
+                // for h in headers {
+                //     html.push_str(&format!("<tht>{h}</th>"));
+                // }
+
+                html.push_str(&format!("</tr></thead>"));
+
+                html.push_str(&format!("<tbody>"));
+
+                for row in cols {
+                    for col in row {
+                        html.push_str(&format!("<tr><td>{col}</td></tr>"));
+                    }
+                }
+
+                html.push_str(&format!("</tbody></table></div>"));
+            }
         }
     }
 
