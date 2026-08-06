fcec78c978d8c1d6cc729706a0b67de35f026330
Miguel Loranca <mloranca703@gmail.com>
2026-08-06 15:43:09 -0600

    gave the main function arguments

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,20 @@ mod renderer;
 use parser::Parser;
 use renderer::HtmlRenderer;
 
+use std::env;
 use std::fs;
 
 fn main() -> std::io::Result<()> {
-    let markdown = fs::read_to_string("src/markdown.md")?;
+    let path: String;
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        path = args[1].clone();
+    } else {
+        path = "src/markdown.md".to_string();
+    }
+
+    let markdown = fs::read_to_string(path)?;
     let ast = Parser::parse(&markdown);
     let html = HtmlRenderer::render(&ast);
     fs::write("parsed.html", html)?;
